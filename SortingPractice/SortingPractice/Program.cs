0194dcce5b9f75d3da6188698f60481c44fcb467
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2023-09-06 16:44:37 -0400

    Lab 3 array sorting method practice.

---
+++ SortingPractice/SortingPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Swap(int[] array, int index0, int index1)
+        {
+            (array[index0], array[index1]) = (array[index1], array[index0]);
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 85, 22, 63, 91, 24 };
+            int max = 0;
+
+            foreach (var item in arr)
+            {
+                if (item > max)
+                    max = item;
+            }
+            Console.WriteLine(max);
+            Swap(arr, Array.IndexOf(arr, max), arr.Length - 1);
+        }
+    }
+}
