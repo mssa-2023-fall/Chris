7373ecc4dfa7a3b279d212e2a9b41ff418cb3bc7
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2023-09-13 16:20:31 -0400

    Customer class that stores name, email, password, and CC. Password hasing method and a quick console app to collect user input(email/password) and compare hash values to grant access.

---
+++ DSA/Hashing/CustomerPasswordHashing/CustomerPasswordHashing/Customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CustomerPasswordHashing
+{
+    public class Customer
+    {
+        //Everything public for initial build. Access modifiers can be fixed later.
+        public string Email {  get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public byte[] Salt { get; set; }
+        public double CreditCard { get; set; }
+        public string PasswordHash { get; set; }
+
+        public Customer(string email, string name, string password, double creditCard)
+        {
+            Email = email;
+            Name = name;
+            Password = password;
+            CreditCard = creditCard;
+        }
+
+        public void GenerateSalt()
+        {
+            Random rand = new Random();
+            var charIzard = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+            Salt = new byte[Password.Length];
+
+            for (int i = 0; i < Password.Length; i++)
+            {
+                Salt[i] = (byte)charIzard[rand.Next(charIzard.Length)];
+            }
+        }
+
+        public void GeneratePasswordHash()
+        {
+            const int keySize = 64;
+            const int iterations = 350000;
+            HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                Encoding.UTF8.GetBytes(Password),
+                Salt,
+                iterations,
+                hashAlgorithm,
+                keySize);
+            PasswordHash = Convert.ToHexString(hash);
+        }
+    }
+}
---
+++ DSA/Hashing/CustomerPasswordHashing/CustomerPasswordHashing/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using CustomerPasswordHashing;
+using System.Security.Cryptography;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Create the dictionary
+        Dictionary<string, Customer> customerDictionary = new Dictionary<string, Customer>();
+      
+        //Add customers
+        Customer customerOne = new Customer("johndoe@gmail.com", "John", "password0", 1111222233334444);
+        Customer customerTwo = new Customer("janesmith@hotmail.com", "Jane", "password1", 1117222233335444);
+        Customer customerThree = new Customer("fred@gmail.com", "Fred", "password2", 2222333344445555);
+        Customer customerFour = new Customer("mikejohnson@yahoo.com", "Mike", "password3",1781422233835432);
+        
+        //Generate password salt
+        customerOne.GenerateSalt();
+        customerTwo.GenerateSalt();
+        customerThree.GenerateSalt();
+        customerFour.GenerateSalt();
+
+        //Generate hashed passwords
+        customerOne.GeneratePasswordHash();
+        customerTwo.GeneratePasswordHash();
+        customerThree.GeneratePasswordHash();
+        customerFour.GeneratePasswordHash();
+
+        //Search customer dictionary
+        customerDictionary.Add(customerOne.Email, customerOne);
+        customerDictionary.Add(customerTwo.Email, customerTwo);
+        customerDictionary.Add(customerThree.Email, customerThree);
+        customerDictionary.Add(customerFour.Email, customerFour);
+
+        //Console prompts to collect email and password
+        Console.WriteLine("Please enter your email:");
+        string userEmail = Console.ReadLine();
+        Console.WriteLine("\nPlease enter your password:");
+        string userPassword = Console.ReadLine();
+        Console.Clear();
+
+
+        //Logic that takes the provided email, assembles a temp customer with userEmail and userPassword
+        //Then searches the customer dictionary for a matching email, gets the password hash
+        //generates a hashed password using the original salt with the provided user password
+        //If the password hashes match, access is granted. If they don't match, "incorrect password"
+        //If the email is not found, "Access denied"
+        if (customerDictionary.TryGetValue(userEmail, out Customer customerIsInDictionary))
+        {
+            byte[] storedSalt = customerIsInDictionary.Salt;
+            Customer tempCustomer = new Customer(userEmail, "", userPassword, 0)
+            {
+                Salt = storedSalt
+            };
+
+            tempCustomer.GeneratePasswordHash();
+
+            if (customerIsInDictionary.PasswordHash == tempCustomer.PasswordHash)
+            {
+                Console.WriteLine("Access granted.");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Access denied.");
+        }
+    }
+}
