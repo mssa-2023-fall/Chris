4690a9c7e956e0a9e417c9a6fd7dc48c7748ec28
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2023-10-31 11:49:04 -0400

    Dependency injection practice

--- ASPDotNet/BasicMiddleware/Program.cs
+++ ASPDotNet/BasicMiddleware/Program.cs
@@ -1,9 +1,32 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Run(async context =>
+app.MapGet("/", async context =>
 {
-    await context.Response.WriteAsync("Hello world!");
+    string prompt;
+
+    do
+    {
+        Console.WriteLine("What would you like to search?");
+        prompt = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(prompt))
+        {
+            string search = $"https://letmegooglethat.com/?q={Uri.EscapeDataString(prompt)}";
+            context.Response.Redirect(search);
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter a valid search term. \n");
+        }
+    }
+    while (string.IsNullOrWhiteSpace(prompt));
+
 });
 
 app.Run();
